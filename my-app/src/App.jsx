4d093e4bf4d8144af1de33074df89dcb20903ab1
hamidjonyoqubov <hamidjonyoqubov2009@gmail.com>
2025-04-05 16:39:22 +0500

    salomsdsdd

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,7 @@
-
+import imgli0 from "./assets/logo.png"
+import imgli3 from "./assets/NAMANGANLIKLAR24.png"
+import imgli1 from "./assets/cif_ru.png"
+import imgli2 from "./assets/nrg-banner.jpg"
 import './App.css'
 
 function App() {
